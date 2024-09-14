49a859f1400f8b2a126d4369e29fa78432a6f59c
notaigbe <notaigbe@gmail.com>
2024-09-14 15:44:21 +0100

    feature: updated blog images

--- detari/settings.py
+++ detari/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ["127.0.0.1:8000", "javiel.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "javiel.onrender.com"]
 
 
 # Application definition
